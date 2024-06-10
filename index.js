bfe21b3fbe0c7f63fbad6c3affc044a48f9c174a
danhdang <anhdang112004@gmail.com>
2024-06-10 11:22:35 +0700

    thêm ảnh, sửa js, sủaw html

--- index.js
+++ index.js
@@ -1,7 +1,51 @@
-/*chưa hoàn thiện*/
-document.querySelectorAll('.game-btn').forEach(button => {
-    button.addEventListener('click', () => {
-        const target = button.getAttribute('data-target');
-        document.getElementById(target).scrollIntoView({ behavior: 'smooth' });
-    });
+// Khởi tạo chỉ số slide và đường dẫn ảnh
+var slideIndex = 1;
+var imgSrc;
+
+// Hàm thay đổi slide
+function plusDivs(n) {
+  // Gọi hàm hiển thị slide với chỉ số mới
+  showDivs((slideIndex += n));
+}
+
+// Hàm hiển thị slide
+function showDivs(n) {
+  var i;
+  // Lấy danh sách các slide
+  var x = document.getElementsByClassName("mySlides");
+  // Nếu chỉ số vượt quá số lượng slide, quay lại slide đầu
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  // Nếu chỉ số nhỏ hơn 1, chuyển đến slide cuối
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  // Ẩn tất cả slide
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  // Hiển thị slide hiện tại
+  x[slideIndex - 1].style.display = "block";
+}
+
+// Hàm thay đổi ảnh khi di chuột vào
+function hover(img, newSrc) {
+  // Lưu lại đường dẫn ảnh gốc
+  imgSrc = img.src;
+  // Thay đổi ảnh
+  img.src = newSrc;
+}
+
+// Hàm khôi phục ảnh gốc khi di chuột ra
+function leave(img) {
+  // Khôi phục ảnh gốc
+  img.src = imgSrc;
+}
+// Hàm chạy khi trang web tải xong
+$(document).ready(function () {
+  // Khi click vào #iw1, hiển thị hoặc ẩn .sub-action
+  $("#iw1").click(function () {
+    $(this).children(".sub-action").slideToggle();
+  });
 });
